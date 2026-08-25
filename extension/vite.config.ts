5ba15cef48beae2ffc1e58cf4bdaef8d86bcad18
ilarikoik <ilari.koik@gmail.com>
2026-08-25 10:32:21 +0300

    tailwind

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json" with { type: "json" };
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [react(), tailwindcss(), crx({ manifest })],
 });
